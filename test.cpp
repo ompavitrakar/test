f4997b70b9fbe7ebcd80100eb046b2b626ad0c8e
ompavitrakar <ompavitrakar@gmail.com>
2026-09-03 15:45:54 +0530

    standard addition of test

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "head.h"
+void fuckoff(int x);
+
+int main()
+{   
+    std::cout << "what? \n";
+    int x{callint()};
+    
+    fuckoff(x);
+    return 0;
+}
+
+void fuckoff(int x)
+{
+    for(int i = 0; i < x; i++)
+    {
+        std::cout << "maKeit yours\n";
+    }
+}
+
